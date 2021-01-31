f6747d397d15db1b47540b3c5881cc63eaad8209
PauSpampinato <rurii.taa@gmail.com>
2021-01-30 21:45:35 -0300

    último commit

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,7 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Haciendo prueba clase 1");
+	alert("Esto funciona de maravilla");
+
 }
 
