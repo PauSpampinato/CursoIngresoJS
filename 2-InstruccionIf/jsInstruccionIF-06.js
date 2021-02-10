57b3d6c7fd8f05e35f5c332165a7f714e2c61b94
PauSpampinato <rurii.taa@gmail.com>
2021-02-10 19:05:17 -0300

    090221

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,23 +1,21 @@
-function mostrar()
-{
+function mostrar() {
 	let edad
 
-	edad=parseInt(document.getElementById("txtIdEdad").value);
+	edad = parseInt(document.getElementById("txtIdEdad").value);
 
-	if (edad < 13){
+	if (edad < 13) {
 		alert("Es niño");
 	}
-	else {
-		if(edad >=13 && edad <=17){
-			alert ("Es adolescente");
-		}
-		else {
-			alert("Es adulto");
-		}
+	else if (edad >= 13 && edad <= 17) {
+		alert("Es adolescente");
 	}
+	else {
+		alert("Es adulto");
+	}
+}
 
 /* También podrían pedirme una condición más como que si es mayor de 65
 diga que es anciano. En ese caso se agrega otro if dentro del último else.
-Queda más ordenado escribir else if continuo.
+Queda más ordenado escribir else if continuo.*/
 
-}//FIN DE LA FUNCIÓN
+//FIN DE LA FUNCIÓN
